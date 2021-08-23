8b0d0e60358d8620c6ce2bbf2882bace6485fe80
Jayajit Das Lab <65561700+jayajitdas@users.noreply.github.com>
2021-08-23 15:19:02 -0400

    Add files via upload

---
+++ model3_act_sig_spparks_src_codes/app_inb_diff_custom.h
@@ -0,0 +1,43 @@
+/* ----------------------------------------------------------------------
+   SPPARKS - Stochastic Parallel PARticle Kinetic Simulator
+   http://www.cs.sandia.gov/~sjplimp/spparks.html
+   Steve Plimpton, sjplimp@sandia.gov, Sandia National Laboratories
+
+   Copyright (2008) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level SPPARKS directory.
+------------------------------------------------------------------------- */
+
+#ifdef APP_CLASS
+AppStyle(inb/diff/custom,AppInbDiffCustom)
+
+#else
+
+#ifndef SPK_APP_INB_DIFF_CUSTOM_H
+#define SPK_APP_INB_DIFF_CUSTOM_H
+
+#include "app_inb_diff.h"
+
+namespace SPPARKS_NS {
+
+class AppInbDiffCustom : public AppInbDiff {
+ public:
+  AppInbDiffCustom(class SPPARKS *, int, char **);
+  ~AppInbDiffCustom() {}
+
+ protected:
+  double total_cluster;
+  double beta;
+  int radius;
+  float ran_com;
+  float periphery_rate;
+  double custom_multiplier(int, int, int, int);
+  };
+
+}
+
+#endif
+#endif
